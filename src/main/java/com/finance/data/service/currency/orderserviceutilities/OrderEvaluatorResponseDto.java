129c77b5be4784335649dbe56d27b7474277dab1
Luke1024 <chajdas.lukasz@gmail.com>
2019-12-21 12:11:42 +0100

    OrderOpeningEvaluator added

--- src/main/java/com/finance/data/service/currency/orderserviceutilities/OrderEvaluatorResponseDto.java
+++ src/main/java/com/finance/data/service/currency/orderserviceutilities/OrderEvaluatorResponseDto.java
@@ -1,4 +1,19 @@
 package com.finance.data.service.currency.orderserviceutilities;
 
 public class OrderEvaluatorResponseDto {
+    private boolean open;
+    private String message;
+
+    public OrderEvaluatorResponseDto(boolean open, String message) {
+        this.open = open;
+        this.message = message;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/com/finance/data/service/currency/orderserviceutilities/OrderOpeningEvaluator.java
+++ src/main/java/com/finance/data/service/currency/orderserviceutilities/OrderOpeningEvaluator.java
@@ -3,7 +3,10 @@ package com.finance.data.service.currency.orderserviceutilities;
 import com.finance.data.domain.accounts.TradingAccount;
 import com.finance.data.domain.currency.CurrencyPairHistoryPoint;
 import com.finance.data.domain.currency.dto.OrderOpeningDto;
+import com.finance.data.domain.currency.enums.LongShort;
+import com.finance.data.service.currency.CalculatingService;
 import com.finance.data.service.currency.CurrencyPairHistoryPointService;
+import com.finance.data.service.currency.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +16,67 @@ public class OrderOpeningEvaluator {
     @Autowired
     private CurrencyPairHistoryPointService currencyPairHistoryPointService;
 
+    @Autowired
+    private OrderService orderService;
 
     public OrderEvaluatorResponseDto evaluate(OrderOpeningDto orderOpeningDto, TradingAccount userTradingAccount) {
-        boolean accountBalance = evaluateAccountBalance(orderOpeningDto, userTradingAccount);
-        boolean takeProfitValueCorrect = evaluateTakeProfit(orderOpeningDto);
-        boolean stopLossValueCorrect = evaluateStopLoss(orderOpeningDto);
+        String message = "";
+        message += evaluateAccountBalance(orderOpeningDto, userTradingAccount);
+        message += evaluateTakeProfitStopLoss(orderOpeningDto);
+        if(message.length()>0){
+            return new OrderEvaluatorResponseDto(false, message);
+        } else {
+            return new OrderEvaluatorResponseDto(true, message);
+        }
+
     }
 
-    private boolean evaluateAccountBalance(OrderOpeningDto orderOpeningDto, TradingAccount userTradingAccount) {
-        CurrencyPairHistoryPoint lastPairPoint =
-                currencyPairHistoryPointService.getLastPointByCurrencyPair(orderOpeningDto.getBaseCurrency);
+    private String evaluateAccountBalance(OrderOpeningDto orderOpeningDto, TradingAccount userTradingAccount) {
+        int leverage = userTradingAccount.getLeverage();
+        double amount = userTradingAccount.getAmount();
+        double lot = orderOpeningDto.getLot();
 
+        if(lot * 100000 > amount * leverage) {
+            return  "";
+        } else {
+            return "Not enough account balance";
+        }
+    }
+
+    private String evaluateTakeProfitStopLoss(OrderOpeningDto orderOpeningDto) {
+        String message = "";
+
+        if(orderOpeningDto.getLongShort() == LongShort.LONG){
+            message += evaluateLong(orderOpeningDto);
+        }
+        if(orderOpeningDto.getLongShort() == LongShort.SHORT){
+            message += evaluateShort(orderOpeningDto);
+        }
+        if(orderOpeningDto.getLongShort() == null){
+            message += "LongShort is null";
+        }
+        return message;
+    }
+
+    private String evaluateLong(OrderOpeningDto orderOpeningDto){
+        String message = "";
+        double takeProfit = orderOpeningDto.getTakeProfit();
+        double stopLoss = orderOpeningDto.getStopLoss();
+
+        if(takeProfit <= stopLoss) {
+            message = "SL cannot be higher than TP";
+        }
+        return message;
+    }
+
+    private String evaluateShort(OrderOpeningDto orderOpeningDto){
+        String message = "";
+        double takeProfit = orderOpeningDto.getTakeProfit();
+        double stopLoss = orderOpeningDto.getStopLoss();
+
+        if(takeProfit >= stopLoss) {
+            message = "SL cannot be lower than TP";
+        }
+        return message;
     }
 }
