2102a2f1db640a7f4231ae8b6d7a92cb6ef311c4
Luke1024 <chajdas.lukasz@gmail.com>
2020-05-01 11:49:18 +0200

    CurrencyPairService rebuild to returning ServiceResponse

--- src/main/java/com/finance/service/database/CurrencyPairService.java
+++ src/main/java/com/finance/service/database/CurrencyPairService.java
@@ -2,34 +2,68 @@ package com.finance.service.database;
 
 import com.finance.domain.CurrencyPair;
 import com.finance.repository.CurrencyPairRepository;
+import com.finance.service.database.communicationObjects.DatabaseEntity;
 import com.finance.service.database.communicationObjects.ServiceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class CurrencyPairService {
     @Autowired
     private CurrencyPairRepository currencyPairRepository;
 
-    public List<CurrencyPair> getCurrencies() {
-        return currencyPairRepository.findAll();
+    public ServiceResponse getCurrencies() {
+        List<CurrencyPair> currencyPairs = currencyPairRepository.findAll();
+        return new ServiceResponse(
+                currencyPairs.stream().map(pair -> (DatabaseEntity) pair)
+                        .collect(Collectors.toList()), "");
     }
 
-    public Optional<CurrencyPair> getCurrencyPair(String currencyPairName) {
-        return currencyPairRepository.findByCurrencyName(currencyPairName);
+    public ServiceResponse getCurrencyPair(String currencyPairName) {
+        return checkIfCurrencyPairNotNull(currencyPairName);
     }
 
-    public void deleteById(Long id){
-        currencyPairRepository.deleteById(id);
+    public ServiceResponse deleteById(Long id){
+        return deleteCurrencyPair(id);
     }
 
     public ServiceResponse saveCurrencyPair(CurrencyPair currencyPair, boolean overwrite) {
         return checkIfCurrencyPairNotNull(currencyPair, overwrite);
     }
 
+
+
+    //getCurrencyPair methods
+    private ServiceResponse checkIfCurrencyPairNotNull(String currencyPairName){
+        if(currencyPairName != null) return findByCurrencyPairName(currencyPairName);
+        else return new ServiceResponse(null, "CurrencyPairName is null");
+    }
+
+    private ServiceResponse findByCurrencyPairName(String currencyPairName){
+        Optional<CurrencyPair> currencyPair = currencyPairRepository.findByCurrencyName(currencyPairName);
+        if(currencyPair.isPresent()) return new ServiceResponse(Arrays.asList(currencyPair.get()), "CurrencyPair found.");
+        else return new ServiceResponse(null, "CurrencyPair not found.");
+    }
+
+    //deleteById methods
+    private ServiceResponse deleteCurrencyPair(Long id){
+        currencyPairRepository.deleteById(id);
+        return checkIfCurrencyStillExists(id);
+    }
+
+    private ServiceResponse checkIfCurrencyStillExists(Long id){
+        Optional<CurrencyPair> currencyPair = currencyPairRepository.findById(id);
+        if(currencyPair.isPresent()) return new ServiceResponse(null, "CurrencyPair id: " + id + " deletion issues.");
+        else return new ServiceResponse(null, "CurrencyPair id: " + id + " removed.");
+    }
+
+
+    //saveCurrencyPair methods
     private ServiceResponse checkIfCurrencyPairNotNull(CurrencyPair currencyPair, boolean overwrite){
         if(currencyPair != null) return checkIfCurrencyPairNameNotNull(currencyPair, overwrite);
         else return new ServiceResponse(null, "CurrencyPair is null.");
